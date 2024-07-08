3f34cd03545cb7f79f1eb4c4170fe174639fefb8
Luis Pires <luisfpiress199@gmail.com>
2024-07-08 11:11:30 -0300

    chore: Update console log statement to use 'body' variable

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ app.post('/api/intermediary', async (req, res) => {
         rejectUnauthorized: false // Permite conexões HTTPS não confiáveis
       })
     });
-    console.log(response.data)
+    console.log(body)
     // Retorna a resposta do Servidor 2 para o cliente (Insomnia)
     res.json(response.data);
   } catch (error) {
