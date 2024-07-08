03fb54d229bc268af5860db0cec377e0e3140bd8
Luis Pires <luisfpiress199@gmail.com>
2024-07-08 10:58:17 -0300

    chore: Add console log statement for debugging purposes

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ app.post('/api/intermediary', async (req, res) => {
         rejectUnauthorized: false // Permite conexões HTTPS não confiáveis
       })
     });
-
+    console.log(data)
     // Retorna a resposta do Servidor 2 para o cliente (Insomnia)
     res.json(response.data);
   } catch (error) {
