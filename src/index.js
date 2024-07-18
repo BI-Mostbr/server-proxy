83108d86f10825eac56866d603bac1b1aace065b
Luis Pires <luisfpiress199@gmail.com>
2024-07-18 17:48:24 -0300

    chore: Update SSL certificate file paths

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const PORT = 3334;
 // Defina o endereço do Servidor
 const SERVER_2_URL = 'http://18.231.162.175:3333/api/intermediary/';
 
-const certificado = fs.readFileSync('/etc/letsencrypt/archive/mostbr.com.br.server-node.mostbr.com.br/cert1.pem');
-const chave = fs.readFileSync('/etc/letsencrypt/archive/mostbr.com.br.server-node.mostbr.com.br/privkey1.pem');
-const ca = fs.readFileSync('/etc/letsencrypt/archive/mostbr.com.br.server-node.mostbr.com.br/chain1.pem');
+const certificado = fs.readFileSync('/etc/letsencrypt/live/mostbr.com.br.server-node.mostbr.com.br/cert.pem');
+const chave = fs.readFileSync('/etc/letsencrypt/live/mostbr.com.br.server-node.mostbr.com.br/privkey.pem');
+const ca = fs.readFileSync('/etc/letsencrypt/live/mostbr.com.br.server-node.mostbr.com.br/chain.pem');
 
 const corsOptions = {
     origin:
